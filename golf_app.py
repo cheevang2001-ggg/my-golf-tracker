2907ec3fa1d8f75f8603166890f1fbcff2cf76af
cheevang2001-ggg <cheevang2001@gmail.com>
2026-02-24 22:00:47 -0600

    Update golf_app.py

--- golf_app.py
+++ golf_app.py
@@ -263,8 +263,8 @@ with tab4:
         # 1. Cash Payout Table
         st.subheader("Cash Payouts")
         payout_data = {
-            "Rank": ["1st Place", "2nd Place", "3rd Place", "4th Place", "Total Cash Payout"],
-            "Amount": ["$200.00", "$140.00", "$100.00", "$60.00", "$500.00"],
+            "Rank": ["1st Place", "2nd Place", "3rd Place", "4th Place", "", "Total Cash Payout"],
+            "Amount": ["$200.00", "$140.00", "$100.00", "$60.00", "", "$500.00"],
             "Note": ["Championship Trophy", "Runner Up", "Third Place", "Fourth Place", "Total Purse Pool"]
         }
         st.table(pd.DataFrame(payout_data))
@@ -374,6 +374,7 @@ with tab6:
         st.caption("CHAMPIONSHIP")
         st.markdown("<h2 style='text-align: center;'>🏆</h2>", unsafe_allow_html=True)
         st.write("Final Match")
+
 
 
 
