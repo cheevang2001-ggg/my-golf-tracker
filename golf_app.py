634e54e9a7ffd9c46ffae4df09cf059f2660f0f0
cheevang2001-ggg <cheevang2001@gmail.com>
2026-03-05 08:45:27 -0600

    Update golf_app.py

--- golf_app.py
+++ golf_app.py
@@ -149,14 +149,26 @@ with tabs[0]: # Scorecard
         else:
             p_data = df_main[df_main['Player'] == player_select]
             w_s = st.selectbox("Select Week", range(1, 15))
-            
+
+            ##################OLD LOGIC########################
             # --- UPDATED HANDICAP APPLICATION LOGIC ---
             # If it's a GGG Event week, force HCP to 0.0. Otherwise, calculate as normal.
-            if w_s in [4, 8, 12]:
-                current_hcp = 0.0
-                st.info("💡 GGG Event: No handicap applied for this round.")
-            else:
-                current_hcp = calculate_rolling_handicap(p_data, w_s)
+            #if w_s in [4, 8, 12]:
+                #current_hcp = 0.0
+                #st.info("💡 GGG Event: No handicap applied for this round.")
+            #else:
+                #current_hcp = calculate_rolling_handicap(p_data, w_s)
+            ##################OLD LOGIC########################
+
+            # Scorecard logic:
+            if w_s in [4, 8]:
+            current_hcp = 0.0
+            st.info("💡 GGG Event: No handicap applied for this round.")
+            elif w_s == 12:
+            current_hcp = calculate_rolling_handicap(p_data, w_s)
+            st.info("🔥 GGG Double Points Event: Rolling handicap is active!")
+        else:
+            current_hcp = calculate_rolling_handicap(p_data, w_s)
             
             h_disp = f"+{abs(current_hcp)}" if current_hcp < 0 else f"{current_hcp}"
             played_rounds = p_data[(p_data['Week'] > 0) & (p_data['DNF'] == False)].sort_values('Week')
@@ -421,6 +433,7 @@ with tabs[6]: # Admin
         if st.button("🚨 Reset Live Board"):
             conn.update(worksheet="LiveScores", data=pd.DataFrame(columns=['Player'] + [str(i) for i in range(1, 10)]))
             st.rerun()
+
 
 
 
