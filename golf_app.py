4c9a8202948f2677a08cb14f27794b9bb8b5304b
cheevang2001-ggg <cheevang2001@gmail.com>
2026-04-12 04:52:59 -0500

    Update golf_app.py

--- golf_app.py
+++ golf_app.py
@@ -371,10 +371,10 @@ with tabs[3]:  # GGG Challenge
         elif challenge_selection == "Gold Card":
             st.subheader("Current Challenge: Gold Card")
             st.markdown("**Entry:** When you purchase the ball challenge before week 1, you automatically get a gold card")
-            st.markdown("**Overview:** Use your Gold Card to play from the front tees. To avoid the penalty, you must finish first overall that week."
-                        "If not, you owe at least $100 worth of food for the end‑of‑year GGGolf Picnic or $100 to GGGolf."
-                        "Example: Dale tunrs his gold card in, Dale plays from the Gold, Dale finish 2nd overall, no handicap used all others scores are handicap. Dale will owe 4 ducks to the picnic"
-                        "Note: The Gold Card cannot be used at Currie Golf Course or during GGGOLF events.")
+            st.markdown("**Overview:** Use your Gold Card to play from the front tees. To avoid the penalty, you must finish first overall that week.
+                        If not, you owe at least $100 worth of food for the end‑of‑year GGGolf Picnic or $100 to GGGolf.
+                        "Example: Dale tunrs his gold card in, Dale plays from the Gold, Dale finish 2nd overall, no handicap used all others scores are handicap. Dale will owe 4 ducks to the picnic
+                        Note: The Gold Card cannot be used at Currie Golf Course or during GGGOLF events.")
             st.divider()
             st.markdown("**How it works**")
             st.markdown("1. Stay tuned for official rules and mechanics.")
