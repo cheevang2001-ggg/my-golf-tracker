cecd3ad1857037e13a5d0fb6e8a0069ed6be3bae
cheevang2001-ggg <cheevang2001@gmail.com>
2026-06-01 11:13:47 -0500

    Update golf_app.py

--- golf_app.py
+++ golf_app.py
@@ -336,7 +336,7 @@ EXISTING_PLAYERS = sorted(df_main['Player'].unique().tolist()) if not df_main.em
 st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
 st.image("GGGOLF-2.png", width=480)
 st.image("2026_Players.jpeg", width=1920) 
-st.markdown("<h1>GGGolf League</h1>", unsafe_allow_html=True)
+st.markdown("<h1>----DEV---GGGolf League----DEV---</h1>", unsafe_allow_html=True)
 st.markdown("</div>", unsafe_allow_html=True)
 
 # Inject Custom CSS to make tabs compact and responsive on mobile screens
